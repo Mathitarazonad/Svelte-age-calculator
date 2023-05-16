0e9e3209bb29953323bbfaa9506b74da6572052c
MathiTarazonad <vladitarazona@gmail.com>
2023-05-16 09:52:33 -0400

    Added function to update the date details

--- src/lib/utils/datesUpdates.ts
+++ src/lib/utils/datesUpdates.ts
@@ -1,17 +1,6 @@
 import dateStore from '$lib/stores/dates.js'
-import type { DateType, FullDate, InputError, SingleDate } from '$lib/types/types.js'
-import { get } from 'svelte/store'
-import { getMaxDayOfMonth } from './dateGetter.js'
-
-export function updateDates (dateToUpate: SingleDate, dateType: DateType): void {
-  const dateStoreValues = get(dateStore)
-  const newDates = { ...dateStoreValues.selectedDates }
-  newDates[dateType] = dateToUpate
-
-  dateStore.update(store => {
-    return { ...store, selectedDates: newDates }
-  })
-}
+import type { DateType, FullDate, InputError, MonthsWithDays, WeeksWithDays } from '$lib/types/types.js'
+import { getMaxDayOfMonth, getStore } from './dateGetter.js'
 
 export function updateFullDate (dates: FullDate): void {
   dateStore.update(store => {
@@ -28,7 +17,7 @@ export function updateDatesErrors (errorType: InputError, dateType: DateType): v
 }
 
 export function updateDateResults (): void {
-  const dateStoreValues = get(dateStore)
+  const dateStoreValues = getStore()
   const { day, month, year } = dateStoreValues.selectedDates
   const newDateResults = calculateDates([day, month, year] as number[])
 
@@ -37,8 +26,87 @@ export function updateDateResults (): void {
   })
 }
 
+export function updateDateDetails (): void {
+  const dataStoreValues = getStore()
+  const includeCurrentDay = dataStoreValues.includeCurrentDay
+  const { day: selectedDay, month: selectedMonth, year: selectedYear } = dataStoreValues.selectedDates
+  const [currentDay, currentMonth, currentYear] = [
+    includeCurrentDay ? (new Date().getDate()) + 1 : new Date().getDate(),
+    (new Date().getMonth()) + 1,
+    new Date().getFullYear()
+  ]
+
+  const getDays = (): number => {
+    const fromDateString = [dataStoreValues.selectedDates.year, dataStoreValues.selectedDates.month, dataStoreValues.selectedDates.day].join('-')
+    const toDateString = [currentYear, currentMonth, currentDay].join('-')
+
+    const fromDate = new Date(fromDateString).getTime()
+    const toDate = new Date(toDateString).getTime()
+
+    const timeDifference = Math.abs(toDate - fromDate)
+    const dayDifference = Math.ceil(timeDifference / (1000 * 60 * 60 * 24))
+
+    return dayDifference
+  }
+
+  const getWeeksAndDays = (): WeeksWithDays => {
+    const weeks = Math.floor(getDays() / 7)
+    const days = Math.floor(getDays()) % 7
+    return { weeks, days }
+  }
+
+  const getMonthsAndDays = (): MonthsWithDays => {
+    let months = 0
+    let days = 0
+
+    if (selectedDay === null || selectedMonth === null || selectedYear === null) {
+      return { months, days }
+    }
+
+    let yearsDifference = currentYear - selectedYear
+
+    if (currentMonth < selectedMonth || (selectedDay > currentDay && currentMonth === selectedMonth)) {
+      yearsDifference -= 1
+    }
+
+    if (yearsDifference <= 1) {
+      return { months, days }
+    }
+
+    months = yearsDifference * 12
+
+    if (selectedMonth <= currentMonth) {
+      months += currentMonth - selectedMonth
+    } else {
+      months += (12 - selectedMonth) + currentMonth
+    }
+
+    if (selectedDay > currentDay) {
+      months -= 1
+    }
+
+    if (selectedDay <= currentDay) {
+      days = currentDay - selectedDay
+    } else {
+      days = ((getMaxDayOfMonth(selectedMonth, selectedYear) - 1) - selectedDay) + currentDay
+    }
+    return { months, days }
+  }
+
+  const newDateDetails = {
+    seconds: getDays() * 86400,
+    minutes: getDays() * 1440,
+    hours: getDays() * 24,
+    days: getDays(),
+    weeksWithDays: getWeeksAndDays(),
+    monthsWithDays: getMonthsAndDays()
+  }
+
+  dateStore.update(store => ({ ...store, dateDetails: newDateDetails }))
+}
+
 function calculateDates (dates: number[]): FullDate {
-  const includeCurrentDay = get(dateStore).includeCurrentDay
+  const includeCurrentDay = getStore().includeCurrentDay
   const [selectedDay, selectedMonth, selectedYear] = dates
   const [currentDay, currentMonth, currentYear] = [
     includeCurrentDay ? (new Date().getDate()) + 1 : new Date().getDate(),
@@ -56,9 +124,7 @@ function calculateDates (dates: number[]): FullDate {
   if (selectedMonth > currentMonth) {
     results.month = (12 - selectedMonth) + currentMonth
 
-    if (selectedDay > currentDay) {
-      results.month--
-    }
+    if (selectedDay > currentDay) results.month--
   } else if (selectedMonth === currentMonth && selectedDay > currentDay) {
     results.month = 11
   } else {
